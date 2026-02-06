a554445cb345f2f5aa3977911215be5ca08bf4fa
ASH <ashrise.x@gmail.com>
2026-02-06 08:13:06 +0500

    Pointer practice

---
+++ L1A13E.cpp
@@ -0,0 +1,21 @@
+//simple pointer variabe
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Enter ta value for a variable:" << endl;
+    int num1;
+    cin >> num1;
+
+    int *numPtr;
+
+
+    
+    cout << numPtr << " is the memory address of the " << num1 << " variable";
+
+    int num2 = 2237;
+    int *num2Ptr;
+
+    cout << num2Ptr << " is the pointer of the variable num2" << num2;
+    
+}
