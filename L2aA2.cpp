4339042c23e0591de5f0000e19f167e110618986
ASH <ashrise.x@gmail.com>
2026-02-20 21:42:02 +0500

    DSA linked list paractice

--- L2aA2.cpp
+++ L2aA2.cpp
@@ -31,6 +31,33 @@ class List{
 
         //method to add a new node
         void insertAtEnd(int data){
-            
+            Node *nn = new Node(data);
+            if(head == NULL){
+                head = nn;
+                return;
+
+            }
+            else {
+                Node *tempPtr = head;
+                while(tempPtr->next != NULL){
+                    tempPtr = tempPtr->next;
+                }
+                tempPtr->next = nn;
+            }
         }
+
+        //display the list
+        void displayList(){
+            Node *temp = head;
+            while(temp != NULL){
+                cout << temp->data << " ";
+                temp = temp->next;
+            }
+            cout << endl;
+
+            return;
+        }
+
+        //merge two lists taking the head of the second list as a parameter
+        void mergeList(No)
 }
